62dacf669f29dbfb28e7428e306e2c131f99a84e
Sajal Sahu <sahu.ss1998@gmail.com>
2025-04-28 23:34:56 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import { pdfjs } from "react-pdf";
+import PaginatedPdfViewer from "./pdfViewer/PaginatedPdfViewer";
+import DummyPdf from "./assets/DummyPdf.pdf";
+import { Grid } from "@mui/material";
+import PdfViewer from "./pdfViewer/PdfViewer";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
+function App() {
+  return (
+    <>
+      <Grid container>
+        <Grid item size={{ sm: 12, md: 6 }}>
+          <PaginatedPdfViewer file={DummyPdf} />
+        </Grid>
+        <Grid item size={{ sm: 12, md: 6 }}>
+          <PdfViewer file={DummyPdf} />
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+
+export default App;
